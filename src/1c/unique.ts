8c800728cecc3d896f2103a1c7ca0d37a65cb86d
Loisteg <loisteg@outlook.com>
2022-04-22 16:47:12 +0300

    Added third variant (unique.ts)

--- src/1c/unique.ts
+++ src/1c/unique.ts
@@ -1,27 +1,33 @@
 // The first variant
 
-const unique = (arg: number[]): number[] => {
-  const result: number[] = [];
+// const unique = (arg: number[]): number[] => {
+//   const result: number[] = [];
 
-  if (arg.length <= 0) return arg;
+//   if (arg.length <= 0) return arg;
 
-  check: for (let i = 0; i < arg.length; i++) {
-    const element: number = arg[i];
+//   check: for (let i = 0; i < arg.length; i++) {
+//     const element: number = arg[i];
 
-    if (arg.lastIndexOf(element) !== i) {
-      continue check;
-    }
+//     if (arg.lastIndexOf(element) !== i) {
+//       continue check;
+//     }
 
-    result.push(element);
-  }
-  return result.sort();
+//     result.push(element);
+//   }
+//   return result.sort();
+// };
+
+// The second variant (if we don't use TypeScript)
+
+// const unique = (arg) => {
+//   return [...new Set(arg)];
+// };
+
+// The third variant
+
+const unique = (arg: number[]) => {
+  return arg.filter((value, index, self) => self.indexOf(value) === index);
 };
-
-// The second variant
-
-// const unique = (arg: number[]) => {
-//     return new Set(arg)
-//   };
 
 console.log(unique([1, 2, 3, 3, 4]));
 console.log(unique([1, 2, 3, 4, 1, 3]));
